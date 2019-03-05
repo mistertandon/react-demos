f7bf5b6e18b4b2e50990d4345556bdeda9dfac1d
mistertandon <enggparveshtandon@gmail.com>
2019-03-05 16:28:29 +0530

    feat: Redux integration with React Part C
    Develop flexbox based css to generate basic layout.

--- 10_redux_integration/src/App.js
+++ 10_redux_integration/src/App.js
@@ -1,11 +1,12 @@
 import React, { Component } from 'react';
 
 import './App.css';
+import './user.css'
 
 import User from './components/User';
 
 class App extends Component {
-  
+
   render() {
     return (
       <div className="App">
--- 10_redux_integration/src/components/User.js
+++ 10_redux_integration/src/components/User.js
@@ -1,6 +1,8 @@
 import React, { Component, Fragment } from 'react';
 import { connect } from 'react-redux';
+
 import { getUserList } from './../actions/userAction';
+import UserRecord from './UserRecord';
 
 class User extends Component {
 
@@ -13,6 +15,25 @@ class User extends Component {
         <h4>
           Hi from User component.
       </h4>
+        <div className="UsersContainer">
+          {
+            users.length && users.map((user, index) => {
+
+              return (
+                <div key={`user_${index}`} className="userRecordContainer">
+
+                  <div className="userRecord">
+                    <UserRecord userProperty={user.first_name} />
+                  </div>
+                  <div className="userRecord">
+                    <UserRecord userProperty={user.last_name} />
+                  </div>
+
+                </div>
+              )
+            })
+          }
+        </div>
       </Fragment>
     );
 
