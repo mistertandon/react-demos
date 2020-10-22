e7948a5743a702f5a53a518a2a6b451678a8e126
mistertandon <enggparveshtandon@gmail.com>
2020-10-22 09:41:46 +0530

    feat: Develop custom hook useHackerNewsApi

--- 17_useeffect_hook_guide_2/src/App.js
+++ 17_useeffect_hook_guide_2/src/App.js
@@ -1,25 +1,110 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useEffect, useState, Fragment } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
+const useHackerNewsApi = () => {
+
+  const [data, setData] = useState({
+    hits: []
+  })
+
+  const [url, setUrl] = useState('https://hn.algolia.com/api/v1/search?query=redux')
+
+  const [isError, setIsError] = useState(false)
+
+  const [isLoading, setIsLoading] = useState(false)
+
+  useEffect(
+    () => {
+
+      const fetchData = async () => {
+
+        setIsLoading(true)
+
+        try {
+
+          const result = await axios(url)
+
+          setData(result.data)
+          setIsError(false)
+        }
+        catch (error) {
+          setIsError(true)
+        }
+
+        setIsLoading(false)
+      }
+
+      fetchData()
+
+    }, [url]
+  )
+
+  return [{ data, isError, isLoading }, setUrl]
+}
+
 function App() {
+
+  const [{ data, isError, isLoading }, doFetch] = useHackerNewsApi()
+
+  const [query, setQuery] = useState('redux')
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Fragment>
+
+      <form onSubmit={(event) => {
+
+        doFetch(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        event.preventDefault()
+      }}>
+
+        <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+
+        <button type="submit">Search</button>
+      </form>
+
+      {
+
+        !isLoading &&
+        isError &&
+        (
+          <div>Error happen...</div>
+        )
+
+      }
+      {
+
+        isLoading &&
+        !isError &&
+        (
+          <div>Loading...</div>
+        )
+      }
+      {
+
+        !isLoading &&
+        !isError &&
+        (
+          <div>
+            {
+              data.hits.map(item => (
+                <li key={item.created_at_i}>
+                  <a href={item.url}>
+                    {
+                      item.title
+                    }
+                  </a>
+                </li>
+              ))
+            }
+
+          </div>
+        )
+
+      }
+
+    </Fragment>
   );
 }
 
