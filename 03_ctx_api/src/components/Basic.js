7e9cf3fde446314ee0b71e9dda36a13c1ca1aeca
mistertandon <enggparveshtandon@gmail.com>
2020-10-22 14:22:46 +0530

    feat: Develop UserContext Component definition
    1. Develop UserContext Component
    2. Develop incrementAgeByOne function definition
    3. Develop UserConext.Provider wrapper inside render function and pass state and functions as a prop

--- 03_ctx_api/src/components/Basic.js
+++ 03_ctx_api/src/components/Basic.js
@@ -1,11 +1,15 @@
 import React, { Fragment } from 'react'
 import Family from './Family'
+import { UserProvider } from './../contexts/UserContext'
 
 const Basic = () => {
 
     return (
+
         <Fragment>
-            <Family />
+            <UserProvider>
+                <Family />
+            </UserProvider>
         </Fragment>
 
     );
