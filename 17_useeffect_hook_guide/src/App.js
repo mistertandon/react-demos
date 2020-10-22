b96614ac0ee3d0442afdea49da1b5b5171362683
mistertandon <enggparveshtandon@gmail.com>
2020-10-22 08:57:33 +0530

    feat: Develop search form definition
    1. Instead of using independent input and button field, develop form definiton

--- 17_useeffect_hook_guide/src/App.js
+++ 17_useeffect_hook_guide/src/App.js
@@ -44,16 +44,17 @@ function App() {
   return (
     <Fragment>
 
-      <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+      <form onSubmit={(event) => {
 
-      <button type="button" onClick={
-        () => (
-          setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
-        )
-      }
-      >
-        Search
-      </button>
+        setUrl(`https://hn.algolia.com/api/v1/search?query=${query}`)
+        event.preventDefault()
+      }}>
+        
+        <input type="text" value={query} onChange={event => setQuery(event.target.value)} />
+
+        <button type="submit">Search</button>
+      </form>
+
       {
 
         !isLoading &&
