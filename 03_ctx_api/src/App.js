4616d8df116cb5e82d36bdddcda5b14687bd8ad4
mistertandon <enggparveshtandon@gmail.com>
2020-10-22 14:10:31 +0530

    feat: Develop basic components to get started with context API
    1. Develop Basic component, render Family child component
    2. Develop Family component, render Person child component
    3. Develop Person component

---
+++ 03_ctx_api/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Basic from './components/Basic'
+
+import './App.css';
+
+function App() {
+
+  return (
+    <div className="App">
+      <Basic />
+    </div>
+  );
+
+}
+
+export default App;
---
+++ 03_ctx_api/src/components/Person.js
@@ -0,0 +1,11 @@
+import React, { Fragment } from 'react'
+
+const Person = () => {
+    return (
+        <Fragment>
+            Hellop from person
+        </Fragment>
+    )
+}
+
+export default Person
