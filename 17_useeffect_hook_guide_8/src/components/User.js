a91f94aace040f3345e85204c662982a5268e9d8
mistertandon <enggparveshtandon@gmail.com>
2020-10-23 17:11:31 +0530

    feat: Set up initial files to get started with redux integration.

---
+++ 17_useeffect_hook_guide_8/src/components/User.js
@@ -0,0 +1,21 @@
+import React, { Component, Fragment } from 'react'
+
+class User extends Component {
+
+    constructor(props) {
+
+        super(props)
+    }
+
+    render() {
+
+        return (
+            <Fragment>
+                Hello from user
+            </Fragment>
+        );
+
+    }
+}
+
+export default User
