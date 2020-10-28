78792e427c413334d8849539eea0e7f9e3e1d2af
mistertandon <enggparveshtandon@gmail.com>
2020-10-28 22:25:10 +0530

    feat: Setup initial file structure to get started with useMemo and useCallBack

---
+++ 17_useeffect_hook_guide_14/src/components/Parent.js
@@ -0,0 +1,12 @@
+import React, { Fragment } from 'react';
+
+const Parent = () => {
+
+    return (
+        <Fragment>
+            Hello from Parent
+        </Fragment>
+    )
+}
+
+export default Parent;
