d55d9a8352bb281674858c83a4e6ad820aa84a8f
mistertandon <enggparveshtandon@gmail.com>
2020-11-18 13:44:07 +0530

    feat: Update counter using hooks.

--- 28_/src/App.js
+++ 28_/src/App.js
@@ -1,23 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState } from 'react';
 
-function App() {
+const App = () => {
+
+  const [count, setCount] = useState(5);
+
+  useEffect(
+    () => {
+
+      if (timeRef) {
+
+        clearInterval(timeRef);
+      }
+
+      const updateCounter = (refCount) => {
+
+        setCount(refCount - 1);
+      }
+
+      if (count > 0) {
+
+        var timeRef = setInterval(function () {
+
+          let countContext = count;
+          updateCounter(countContext);
+        },
+          2000);
+      }
+
+    }, [count]
+  )
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        count
+      }
     </div>
   );
 }
