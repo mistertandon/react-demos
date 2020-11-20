0c5b46367e189188e676cbafb482f3416b8f5b35
mistertandon <enggparveshtandon@gmail.com>
2020-11-20 18:27:18 +0530

    feat: Develop action definition.
    1. GET_SPACEX_DATA_ALL
    2. GET_SPACEX_DATA_STATUS_SUCCESS
    3. GET_SPACEX_DATA_STATUS_ERROR

---
+++ 28_/src/redux/actions/spacex.action.js
@@ -0,0 +1,23 @@
+import { GET_SPACEX_DATA_ALL, GET_SPACEX_DATA_STATUS_SUCCESS, GET_SPACEX_DATA_STATUS_ERROR } from './spacex.constant';
+
+export const getSpaceXDataAll = () => {
+
+    return {
+        type: GET_SPACEX_DATA_ALL
+    }
+}
+
+export const fecthSpaceXSuccess = (data) => {
+
+    return {
+        type: GET_SPACEX_DATA_STATUS_SUCCESS,
+        data
+    }
+}
+export const fecthSpaceXError = (error) => {
+
+    return {
+        type: GET_SPACEX_DATA_STATUS_ERROR,
+        error
+    }
+}
