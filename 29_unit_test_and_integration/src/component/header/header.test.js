b6c0571a4a0dab07d8846ea058022defef672aeb
mistertandon <enggparveshtandon@gmail.com>
2020-12-18 21:44:41 +0530

    feat: Implement beforeEach function definition in header.test.js

--- 29_unit_test_and_integration/src/component/header/header.test.js
+++ 29_unit_test_and_integration/src/component/header/header.test.js
@@ -3,11 +3,25 @@ import { shallow } from 'enzyme';
 
 import Header from './header';
 
+const setUp = (props = {}) => {
+
+    const component = shallow(<Header />);
+
+    return component
+}
+
 describe('Header Component', () => {
+
+    let component;
+
+    beforeEach(() => {
+
+        component = setUp();
+    });
 
     it('Should render without any error', () => {
 
-        const component = shallow(<Header />);
+        // const component = shallow(<Header />);
         const wrapper = component.find(`[data-test='header-component-container']`);
 
         expect(wrapper.length).toBe(1);
@@ -15,7 +29,7 @@ describe('Header Component', () => {
 
     it('Should render header', () => {
 
-        const component = shallow(<Header />);
+        // const component = shallow(<Header />);
         const wrapper = component.find(`[data-test='header-container']`);
 
         expect(wrapper.length).toBe(1);
@@ -23,7 +37,7 @@ describe('Header Component', () => {
 
     it('Should render without any error', () => {
 
-        const component = shallow(<Header />);
+        // const component = shallow(<Header />);
         const wrapper = component.find(`[data-test='header-logo']`);
 
         expect(wrapper.length).toBe(1);
